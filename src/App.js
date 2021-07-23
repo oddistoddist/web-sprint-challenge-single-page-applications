8df63596412654b7f1b58f01b25a0f37b12e10e3
Lee Scott <Odda@MacBook-Pro-2.local>
2021-07-23 17:51:46 -0400

    added pizza form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import PF from './components/pizzForm'
 
 const App = () => {
+  //setting up state for mapping later and passing it into pizzaform
+  const [order, setorder] = useState([])
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <PF />
     </>
   );
 };
---
+++ src/components/pizzForm.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+export default (props) => {
+    return(
+        <div>
+            pf
+        </div>
+    )
+}
